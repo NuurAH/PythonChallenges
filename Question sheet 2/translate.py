f176db1d632c20ffb2c6ff862f7214e33cb81490
Harbi Nuur <harbi.nuur@postgrad.manchester.ac.uk>
2025-02-09 00:27:51 +0000

    Update translate.py
    whoops wrong code snippet. These are the main changes

--- Question sheet 2/translate.py
+++ Question sheet 2/translate.py
@@ -1,37 +1,75 @@
-#variable list
-dna = "GCTGAGACTTCCTGGACGGGGGACAGGCTGTGGGGTTTCTCAGATAACTGGGCCCCTGCGCTCAGGAGGCCTTCACCCTCTGCTCTGGGTAAAGTTCATTGGAACAGAAAGAAATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAATGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTGAAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGATATAACCAAAAGGAGCCTACAAGAAAGTACGAGATTTAGTCAACTTGTTGAAGAGCTATTGAAAATCATTTGTGCTTTTCAGCTTGACACAGGTTTGGAGTATGCAAACAGCTATAATTTTGCAAAAAAGGAAAATAACTCTCCTGAACATCTAAAAGATGAAGTTTCTATCATCCAAAGTATGGGCTACAGAAACCGTGCCAAAAGACTTCTACAGAGTGAACCCGAAAATCCTTCCTTGCAGGAAACCAGTCTCAGTGTCCAACTCTCTAACCTTGGAACTGTGAGAACTCTGAGGACAAAGCAGCGGATACAACCTCAAAAGACGTCTGTCTACATTGAATTGGGATCTGATTCTTCTGAAGATACCGTTAATAAGGCAACTTATTGCAGTGTGGGAGATCAAG"
+#make the left side a key with the key value being on the right
+#always keep multiple values in an array per key 
+codon_table = {
+    "F" : ["TTT","TTC"],
+    "L" : ["TTA", "TTG", "CTT", "CTA", "CTG" ],
+    "I" : ["ATT", "ATC", "ATA"],
+    "M" : ["ATG"],
+    "V" : ["GTT", "GTC", "GTG"],
+    "S" : ["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"],
+    "P" : ["CCT", "CCC", "CCA", "CCG"],
+    "T" : ["ACT", "ACC", "ACA", "ACG"],
+    "A" : ["GCT", "GCC", "GCA", "GCG"],
+    "Y" : ["TAT", "TAC"],
+    "*" : ["TAA", "TAG", "TGA"],
+    "H" : ["CAT", "CAC"],
+    "Q" : ["CAA", "CAG"],
+    "N" : ["AAT", "AAC"],
+    "K" : ["AAA", "AAG"],
+    "D" : ["GAT", "GAC"],
+    "E" : ["GAA", "GAG"],
+    "C" : ["TGT", "TGC"],
+    "W" : ["TGG"],
+    "R" : ["CGT", "CGC", "CGA", "CGG", "AGA", "AGG" ],
+    "G" : ["GGT", "GGC", "GGA", "GGG" ]
+}
+#variables (the usual suspects)
+dna = "aggagtaagcccttgcaactggaaatacacccattg"
 dna_length = len(dna)
-block_size = 10
-blocking = 10
+block_size = 3
 start = 0
-dna_list = []
+dna_list = [ ]
+extracted_codons = [ ]
+protein = 0
+dna_upper = dna.upper()
+split_dna_list = []
+
+#function   
+print(dna_upper)
+def find():
+    print(dna_list[-1])
+    for key, value in codon_table.items():
+        if dna_list[-1] in value:
+            #print(value)
+            extracted_codons.append(key)
 
 
-#loops and functions
-def new_line():
-   if start%60 == 0 and start < dna_length:
-      dna_list.append("\n" + str(start + 1))
+
+#need to find a way to make the find function not return all of the previous values in the loop. So make it jump by 3
 
 while start < dna_length :
-   if start == 0:
-      dna_list.insert (0, str(1))
-   dna_list.append(dna[start:blocking]) 
-   start += block_size
-   blocking += block_size
-   new_line()
+    dna_list.append(dna_upper[start:(start+block_size)]) 
+    #print(dna_list[-1])
+    find()
+    start += block_size
 
-split_dna = ""
-for _ in dna_list:
-   if start == 0 or start%60 == 0:
-      split_dna += ((_) + " ") 
-   else:
-      split_dna += (" " + (_) + " ") 
+print(extracted_codons)
+   
+# #splitting dna
+# split_dna = ""
+# for _ in dna_list:
+#    split_dna += (" " + (_) + " ")
 
 
+# #need to convert split dna into values so if 
+# #For split on the list, it needs to be converted into the right key
+
+# #printing the new list
+
+protein_list = ""
+for x in extracted_codons:
+    protein_list += x
 
 
-#printing
-formatted_dna = split_dna.strip()
-print(formatted_dna.lower())
-
-
+print(protein_list)
+#ctrl forward slash comments sections and can uncomment
