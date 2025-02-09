b6ca92ba7a7835e6a80be403a64d7a0457896992
Harbi Nuur <harbi.nuur@postgrad.manchester.ac.uk>
2025-02-09 00:27:11 +0000

    Update translate.py
    Made a lot of changes to make the logic of the code work better. Turns out the main issue was due to the dictionary

--- Question sheet 2/translate.py
+++ Question sheet 2/translate.py
@@ -1,114 +1,37 @@
-codon_table = {
-    "F" : "TTT",
-    "F" : "TTC",
-    "L" : "TTA",
-    "L" : "TTG", 
-    "L" : "CTT", 
-    "L" : "CTA",
-    "L" : "CTG" ,
-    "I" : "ATT",
-    "I" : "ATC",
-    "I" : "ATA",
-    "M" : "ATG",
-    "V" : "GTT",
-    "V" : "GTC",
-    "V" : "GTG",
-    "S" : "TCT",
-    "S" : "TCC", 
-    "S" : "TCA",
-    "S" : "TCG",
-    "S" : "AGT",
-    "S" : "AGC",
-    "P" : "CCT", 
-    "P" : "CCC",
-    "P" : "CCA",
-    "P" : "CCG",
-    "T" : "ACT",
-    "T" : "ACC",
-    "T" : "ACA",
-    "T" : "ACG",
-    "A" : "GCT", 
-    "A" : "GCC",
-    "A" : "GCA",
-    "A" : "GCG",
-    "Y" : "TAT",
-    "Y" : "TAC",
-    "*" : "TAA", 
-    "*" : "TAG", 
-    "*" : "TGA",
-    "H" : "CAT",
-    "H" : "CAC",
-    "Q" : "CAA", 
-    "Q" : "CAG",
-    "N" : "AAT",
-    "N" : "AAC",
-    "K" : "AAA", 
-    "K" : "AAG",
-    "D" : "GAT", 
-    "D" : "GAC",
-    "E" : "GAA",
-    "E" : "GAG",
-    "C" : "TGT",
-    "C" : "TGC",
-    "W" : "TGG",
-    "R" : "CGT",
-    "R" : "CGC",
-    "R" : "CGA",
-    "R" : "CGG",
-    "R" : "AGA",
-    "R" : "AGG",
-    "G" : "GGT", 
-    "G" : "GGC",
-    "G" : "GGA",
-    "G" : "GGG" 
-}
-#variables (the usual suspects)
-dna = "aggagtaagcccttgcaactggaaatacacccattg"
+#variable list
+dna = "GCTGAGACTTCCTGGACGGGGGACAGGCTGTGGGGTTTCTCAGATAACTGGGCCCCTGCGCTCAGGAGGCCTTCACCCTCTGCTCTGGGTAAAGTTCATTGGAACAGAAAGAAATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAATGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTGAAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGATATAACCAAAAGGAGCCTACAAGAAAGTACGAGATTTAGTCAACTTGTTGAAGAGCTATTGAAAATCATTTGTGCTTTTCAGCTTGACACAGGTTTGGAGTATGCAAACAGCTATAATTTTGCAAAAAAGGAAAATAACTCTCCTGAACATCTAAAAGATGAAGTTTCTATCATCCAAAGTATGGGCTACAGAAACCGTGCCAAAAGACTTCTACAGAGTGAACCCGAAAATCCTTCCTTGCAGGAAACCAGTCTCAGTGTCCAACTCTCTAACCTTGGAACTGTGAGAACTCTGAGGACAAAGCAGCGGATACAACCTCAAAAGACGTCTGTCTACATTGAATTGGGATCTGATTCTTCTGAAGATACCGTTAATAAGGCAACTTATTGCAGTGTGGGAGATCAAG"
 dna_length = len(dna)
-block_size = 3
-blocking = 3
+block_size = 10
+blocking = 10
 start = 0
-dna_list = [ ]
-extracted_codons = [ ]
-protein = 0
-dna_upper = dna.upper()
-split_dna_list = []
-second_list = []
-
-#function   
-print(dna_upper)
-def find():
-   for key, value in codon_table.items():
-      if value in second_list:
-            extracted_codons.append(key)
-            print(second_list)
-            
+dna_list = []
 
 
-#need to find a way to make the find function not return all of the previous values in the loop. So make it jump by 3
+#loops and functions
+def new_line():
+   if start%60 == 0 and start < dna_length:
+      dna_list.append("\n" + str(start + 1))
+
 while start < dna_length :
-   dna_list.append(dna_upper[start:blocking]) 
-   second_list.append(dna_upper[start:blocking])
+   if start == 0:
+      dna_list.insert (0, str(1))
+   dna_list.append(dna[start:blocking]) 
    start += block_size
    blocking += block_size
-   if len(second_list) == 1:
-      find()
-   else:
-       continue
-#splitting dna
+   new_line()
+
 split_dna = ""
 for _ in dna_list:
-   split_dna += (" " + (_) + " ")
-print(split_dna)
-
-#need to convert split dna into values so if 
-#For split on the list, it needs to be converted into the right key
-
-#printing the new list
-print(extracted_codons)
-protein_list = ""
-for x in extracted_codons:
-    protein_list += x
+   if start == 0 or start%60 == 0:
+      split_dna += ((_) + " ") 
+   else:
+      split_dna += (" " + (_) + " ") 
 
 
-print(protein_list)
+
+
+#printing
+formatted_dna = split_dna.strip()
+print(formatted_dna.lower())
+
+
