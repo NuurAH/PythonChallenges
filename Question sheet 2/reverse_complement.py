ee4a4f2b1b57ad60cfecf366181fb7dec2fe36cd
Harbi Nuur <harbi.nuur@postgrad.manchester.ac.uk>
2025-03-17 16:25:26 +0000

    Update reverse_complement.py
    I'm having a bad time

--- Question sheet 2/reverse_complement.py
+++ Question sheet 2/reverse_complement.py
@@ -1,30 +1,67 @@
-dna = "aggagtaagcccttgcaactggaaatacacccattg"
+dna = "XXX"
 reverse_dna = dna[::-1]
 dna_length = len(dna)
+c_list = list(dna)
 start = 0
 dna_list = []
 complement_list = []
+tracker = 0
 
 complement_table = {
-    "a" : ["t"],
-    "t" : ["a"],
-    "g" : ["c"],
-    "c" : ["g"] 
+    "a" : "t",
+    "t" : "a",
+    "g" : "c",
+    "c" : "g",
+    "A" : "T",
+    "T" : "A",
+    "G" : "C",
+    "C" : "G"
 }
+
 #repurposing the function to look through the last string value in the DNA to look through the complement table
 def reverse_complement():
+    tracker = 0
     for key, value in complement_table.items():
-        if dna_list[-1] in value:
+        if dna_list[-1] in value :
+            tracker +=1
             complement_list.append(key)
+        else:
+            print("This is not a base" + " " + dna_list[-1])
 
-#same kind of logic as translating I think
-while start < dna_length :
+
+while start < dna_length:
     dna_list.append(dna[start])
     reverse_complement()
-    start += 1
+    start +=1
+
+# def base_scan():
+#     for value in complement_table.items():
+#         if dna_list[-1] not in value:
+#             print("this is not a value")
+# different function?
+
+    
+#same kind of logic as translating I think
+# while start < dna_length :
+#   if c_list[start] == "A":
+#       c_list[start] == "T"
+#   elif c_list[start] == "T":
+#       c_list[start] == "A"
+#   elif c_list[start] == "G":
+#       c_list[start] == "C"
+#   elif c_list[start] == "C":
+#     c_list[start] == "G"
+#   else:
+#     print("This is not a base")
+# complement_list.append(c_list(start))
+# start += 1
+
 
 
 #very easy to reverse the string so I saved it till the end rather than mess with a reversed DNA string
 complement_output = ""
 for x in complement_list:
     complement_output += x
+
+
+print(complement_output[::-1])
